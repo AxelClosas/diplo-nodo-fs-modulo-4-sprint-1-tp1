1b12813bbbb7da47a021ce81cd707fbcf9c5fde4
Axel Closas <axelclosas13@gmail.com>
2025-10-06 14:03:43 -0300

    fix: Ahora se muestra el menú al accionar el boton del menú en mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,50 +16,68 @@ const NavBar = () => {
   const toggleMenu = () => setIsOpen(!isOpen)
 
   return (
-    <nav className="flex items-center justify-center">
-      {/* Burger btn */}
+    <>
+      <nav className="flex items-center justify-center">
+        {/* VIEW DESKTOP */}
+        <button
+          className="md:hidden text-white p-2 cursor-pointer"
+          onClick={toggleMenu}
+        >
+          {console.log(isOpen)}
 
-      <button
-        className="md:hidden text-white p-2 cursor-pointer"
-        onClick={toggleMenu}
-      >
-        {console.log(isOpen)}
+          {/* Uso de iconos SVG para abrir y cerrar menú hamburguesa */}
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24 "
+            >
+            { isOpen
+              ? <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12" // X
+                />
+              : <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16" // Burger
+                />
+            }
+          </svg>
+        </button>
+        <div>
+          <ul className="hidden md:flex gap-2">
+            { navbarLinks.map((item, index) => (
+                <li key={index}>
+                  <a href={item.link}
+                    className="text-white font-semibold py-2 px-4 rounded hover:bg-[rgba(255,255,255,0.2)]"
+                  >{item.title}
+                  </a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
 
-        {/* Uso de iconos SVG para abrir y cerrar menú hamburguesa */}
-        <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24 "
-          >
-          { isOpen
-            ? <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12" // X
-              />
-            : <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16" // Burger
-              />
-          }
-        </svg>
-      </button>
-        <ul className="hidden md:flex gap-2">
-          { navbarLinks.map((item, index) => (
-              <li key={index}>
+        {/* Mobile Menu */}
+        <div className={`md:hidden absolute w-full top-24 left-0 bg-gray-800/80 ${isOpen ? 'block' : 'hidden'} `}>
+          <ul className="flex flex-col items-center">
+            { navbarLinks.map((item, index) => (
+              <li key={index} className="w-full text-center">
                 <a href={item.link}
-                  className="text-white font-semibold py-2 px-4 rounded hover:bg-[rgba(255,255,255,0.2)]"
+                  className="block w-full text-white font-semibold py-4 px-4 transition-all duration-150 hover:bg-[rgba(255,255,255,0.2)]"
                 >{item.title}
                 </a>
               </li>
-            ))
-          }
-        </ul>
+              ))
+            }
+          </ul>
+        </div>
       </nav>
+    </>
   )
 }
 
