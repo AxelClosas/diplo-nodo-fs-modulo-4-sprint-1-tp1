f4da63f9a41c9e2a2e2028e16d6969f4a62ca4c0
Axel Closas <axelclosas13@gmail.com>
2025-10-07 09:54:35 -0300

    fix: Se agregó un margin top para separar el footer del contenido principal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-gray-800 text-white py-4 mt-8">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} Club Alianza Handball Catamarca. Todos los derechos reservados.</p>
         <p>Hecho con React por <a href="https://github.com/AxelClosas" className='text-blue-400 font-bold hover:text-blue-200' target="_blank" rel="noopener noreferrer">Axel Closas</a></p>
